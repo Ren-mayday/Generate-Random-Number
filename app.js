34f850c89839bbe27d923d0b6115675c122fefd6
Ren-mayday <rencel.dayrit@gmail.com>
2021-09-26 12:44:39 +0200

    Random Number Project

---
+++ app.js
@@ -0,0 +1,26 @@
+window.onload = function () {
+  debugger;
+  document.getElementById("generateButton").onclick = generateRandomNumber;
+};
+
+function generateRandomNumber() {
+  debugger;
+  const minimum = document.getElementById("minimum").value;
+  const maximum = document.getElementById("maximum").value;
+
+  const randomNumber = Math.floor(
+    Math.random() * (maximum - minimum) + minimum
+  );
+  updateDisplayNumber(randomNumber);
+}
+
+function updateDisplayNumber(randomNumber) {
+  document.getElementById("result").innerHTML = randomNumber;
+}
+
+//how it works debbuger --> it stops the function until the debbuger line and it provides you the whole function or you could indicate brekpoints so you can see your code behaviour until the breakpoint you indicate.
+//finish function
+//value string = number
+
+/* let randomMinimumNumber = Math.floor(Math.random());
+  let randomMaximumNumber = Math.floor(Math.random()); */
